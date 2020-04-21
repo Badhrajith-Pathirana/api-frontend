9f6d0160daf47dd18e19abc4703c82c58ba767f8
Badhrajith-Pathirana <badhrajith.pathirana@gmail.com>
2020-04-21 23:26:32 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CreateLoginComponent} from './create-login/create-login.component';
+import {CreateSignupComponent} from './create-signup/create-signup.component';
+import {HeaderComponent} from './header/header.component';
+import {FootarComponent} from './footar/footar.component';
+import {SidebarNavComponent} from './sidebar-nav/sidebar-nav.component';
+import {AppRoutingModule} from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateLoginComponent,
+    CreateSignupComponent,
+    HeaderComponent,
+    FootarComponent,
+    SidebarNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
