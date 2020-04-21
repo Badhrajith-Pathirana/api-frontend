9ed5c16bd192adc5a1b8e2d4e622d6b6e0c27d43
Badhrajith-Pathirana <badhrajith.pathirana@gmail.com>
2020-04-22 00:04:59 +0530

    Script for login check added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'api-frontend';
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.routeTheLogin();
+  }
+
+  routeTheLogin() {
+    const user_type = localStorage.getItem('user-type');
+
+    if (user_type !== null && user_type  !== undefined) {
+      switch (user_type) {
+        case 'EXPERT':
+          // Expert router adds here
+          break;
+        case 'STUDENT':
+      //    Student router adds here
+          break;
+        case 'PROGRAM_MANAGER':
+      //    pm router here
+          break;
+      }
+    } else {
+      this.router.navigate(['create-post']);
+    }
+  }
+
 }
