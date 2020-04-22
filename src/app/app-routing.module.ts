2c0a7dcfe7a0ed3a2e23f7ca237051153b8a973b
Chamalki97 <gachamalki@gmail.com>
2020-04-22 20:20:23 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {CreatePostComponent} from './create-post/create-post.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
+import { CreateSignupComponent } from './create-signup/create-signup.component';
+import { CreateLoginComponent } from './create-login/create-login.component';
 
 
 const routes: Routes = [
   {path: 'create-post' , component: CreatePostComponent},
-  {path: 'user-profile/**', component: UserProfileComponent}
+  {path: 'user-profile/**', component: UserProfileComponent},
+  {path: 'create-signup' , component: CreateSignupComponent},
+  {path: 'create-login' , component: CreateLoginComponent}
 ];
 
 @NgModule({
